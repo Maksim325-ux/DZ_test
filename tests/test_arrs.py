c6184904fba3099aa950608ebe2c35e107dd4d46
Max <maks_orlov_2015@mail.ru>
2023-02-26 14:11:16 +1000

    исправление тестов test_slice импортирование фикстур

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,20 @@
 from utils import arrs
+import pytest
+
+@pytest.fixture
+def list_test():
+    return [1, 2, 3]
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
-    assert arrs.get([], -1, "test") is "test"
+def test_get(list_test):
+    assert arrs.get(list_test, 1, "test") == 2
+    assert arrs.get([], -1, "test") == "test"
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+def test_slice(list_test):
+    assert arrs.my_slice(list_test, 1, 3) == [2, 3]
+    assert arrs.my_slice(list_test, 1) == [2, 3]
+    assert arrs.my_slice(list_test, 0, -1) == [1, 2]
+    assert arrs.my_slice([], 0, -1) == []
+    assert arrs.my_slice(list_test, -1, -3) == []
+    assert arrs.my_slice(list_test, -4) == [1, 2, 3]
